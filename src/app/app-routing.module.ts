bba1ee6f89592a4d37788a68d6d2d16ccfa22417
takeshi <takeshi@test.com>
2022-10-19 15:27:33 -0300

    Inicio do Projeto Final

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { ExclusaoViagemComponent } from './exclusao-viagem/exclusao-viagem.component';
+import { DetalhesViagemComponent } from './detalhes-viagem/detalhes-viagem.component';
+import { ListagemViagemComponent } from './listagem-viagem/listagem-viagem.component';
+import { CadastroViagemComponent } from './cadastro-viagem/cadastro-viagem.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cadastro', component: CadastroViagemComponent},
+  {path: 'lista', component: ListagemViagemComponent},
+  {path: 'detalhes', component: DetalhesViagemComponent},
+  {path: 'excluir', component: ExclusaoViagemComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
